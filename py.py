c9b953078e43d6316fb29975b47609ec7be2b74d
Aashish <8xdeadshot@gmail.com>
2023-10-23 19:35:59 +0530

    minor change

--- py.py
+++ py.py
@@ -5,19 +5,23 @@ for dirpath, dirnames, filenames in os.walk("."):
         index_file.write('<html>\n<head><link rel="stylesheet" type="text/css" href="/style.css">\n<title>Algorithm & Programs</title>\n</head>\n<body>\n')  # Opening body tag here
 
         for filename in filenames:
-            if filename != "index.html" and filename.endswith(".txt"):  # Exclude the index file and .txt files from the listing
+            if filename != "index.html" and filename.endswith(".txt"):
                 filename_without_extension, _ = os.path.splitext(filename)
                 index_file.write(f'<a href="{filename}">{filename_without_extension}</a>\n')
 
-        index_file.write('</body>\n</html>\n')  # Closing body tag here
+        index_file.write('</body>\n</html>\n')
 
 # <html>
 # <head>
+# <meta property="og:title" content="Lab Programs and Algorithms" />
+# <meta property="og:description" content="Java & C" />
+# <meta property="og:image" content="https://i.pinimg.com/564x/1c/54/f7/1c54f7b06d7723c21afc5035bf88a5ef.jpg" />
+# <meta property="og:url" content="https://aashishvinu.me/"/>
 # <link rel="stylesheet" type="text/css" href="/style.css">
 # <title>Algorithm & Programs</title>
 # </head><body>
-# <h1>Algorithms and Program</h1>
-# <a href="/c">C program</a><br>
-# <a href="/j">Java</a><br>
+# <a href="/c">C program</a>
+# <a href="/j">Java</a>
 # </body>
 # </html>
+
