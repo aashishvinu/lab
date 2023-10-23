38ed56794a6b4c23947adf2cf9ed1be1fe5be996
Aashish <8xdeadshot@gmail.com>
2023-10-23 19:11:03 +0530

    major edit

--- py.py
+++ py.py
@@ -2,6 +2,22 @@ import os
 
 for dirpath, dirnames, filenames in os.walk("."):
     with open(os.path.join(dirpath, "index.html"), "w") as index_file:
+        index_file.write('<html>\n<head><link rel="stylesheet" type="text/css" href="/style.css">\n<title>Algorithm & Programs</title>\n</head>\n<body>\n')  # Opening body tag here
+
         for filename in filenames:
-            if filename != "index.html":  # Exclude the index file from the listing
-                index_file.write(f'<a href="{filename}">{filename}</a><br>\n')
+            if filename != "index.html" and filename.endswith(".txt"):  # Exclude the index file and .txt files from the listing
+                filename_without_extension, _ = os.path.splitext(filename)
+                index_file.write(f'<a href="{filename}">{filename_without_extension}</a><br>\n')
+
+        index_file.write('</body>\n</html>\n')  # Closing body tag here
+
+# <html>
+# <head>
+# <link rel="stylesheet" type="text/css" href="/style.css">
+# <title>Algorithm & Programs</title>
+# </head><body>
+# <h1>Algorithms and Program</h1>
+# <a href="/c">C program</a><br>
+# <a href="/j">Java</a><br>
+# </body>
+# </html>
